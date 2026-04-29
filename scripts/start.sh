57ecbde4b6edea279acf1118e27563b55ffd3f5c
EC2 Default User <ec2-user@ip-172-31-76-130.ec2.internal>
2026-04-29 09:15:41 +0000

    fix script deploy

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
-cd /home/ec2-user/app
-docker-compose up -d --build
+
+echo "Iniciando deploy..."
+
+cd /home/ec2-user/app || exit 1
+
+sudo systemctl start docker
+
+sudo docker-compose down
+sudo docker-compose up -d --build
+
+echo "Deploy completado"
 
